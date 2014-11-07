33a60275c676a2b803ee901d1488011287e9874e
Missichini Julien <adm-2w22a-06.univ-avignon.fr>
2014-11-07 15:43:28 +0100

    Remplir la gamelle

---
+++ remplir.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "nourriture.hpp"
+
+using namespace std;
+
+remplir::remplir()
+{
+	eau = 0;
+	nourriture = 0;
+}
+
+remplir::~remplir()
+{}
+
+remplir::remplirEau()
+{
+	eau = 100;
+}
+
+replir::remplirNourriture()
+{
+	nourriture = 100;
+}
