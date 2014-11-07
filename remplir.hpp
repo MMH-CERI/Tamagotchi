b1e2b62a9c83bd4762ecbf730eb3ebdc1be58320
Missichini Julien <adm-2w22a-06.univ-avignon.fr>
2014-11-07 15:44:10 +0100

    header remplir la gamelle

---
+++ remplir.hpp
@@ -0,0 +1,20 @@
+#ifndef REMPLIR_HPP
+#define REMPLIR_HPP
+
+class remplir
+{
+
+	private:
+	
+		int eau;
+		int nourriture;
+
+	public:
+
+		remplir();
+		~remplir();
+		void remplirEau();
+		void remplirNourriture();
+}
+
+#endif
